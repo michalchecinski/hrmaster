fd8226a55919eb51cc05529bde996c4ef870e3c9
Michał Chęciński <michal.checinski@outlook.com>
2018-10-12 11:23:29 +0200

    Add clean ASP.NET Core MVC project

---
+++ src/HRMasterASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HRMasterASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
