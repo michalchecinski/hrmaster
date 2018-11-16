e76d0c7dbdded5a5fde072bef3eb7b30b09820df
Michał Chęciński <michal.checinski@outlook.com>
2018-11-16 21:41:35 +0100

    Add joboffer

---
+++ src/HRMasterASP/Controllers/HttpStatusCodeResult.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRMasterASP.Controllers
+{
+    internal class HttpStatusCodeResult : ActionResult
+    {
+        private object badRequest;
+
+        public HttpStatusCodeResult(object badRequest)
+        {
+            this.badRequest = badRequest;
+        }
+    }
+}
---
+++ src/HRMasterASP/Models/JobOffer.cs
@@ -0,0 +1,34 @@
+﻿using HRMasterASP.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRMasterASP.Models
+{
+    public class JobOffer
+    {
+        public int Id { get; set; }
+        [Display(Name = "Job title")]
+        [Required]
+        public string JobTitle { get; set; }
+        public virtual Company Company { get; set; }
+        public virtual int CompanyId { get; set; }
+        [Display(Name = "Salary from")]
+        public decimal? SalaryFrom { get; set; }
+        [Display(Name = "Salary to")]
+        public decimal? SalaryTo { get; set; }
+        public DateTime Created { get; set; }
+        public string Location { get; set; }
+        [Required]
+        [MinLength(100)]
+        public string Description { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode =true, DataFormatString ="{0:yyy-MM-dd}")]
+        [Display(Name ="Valid until")]
+        public DateTime? ValidUntil { get; set; }
+        public List<JobApplication> JobApplications { get; set; } = new List<JobApplication>();
+
+    }
+}
