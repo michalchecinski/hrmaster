d6b2cc81e39562428572a36c775a497b3808e27f
Michał Chęciński <michal.checinski@outlook.com>
2018-12-08 19:01:05 +0100

    Add missing props and attribs

--- src/HRMasterASP/Models/JobApplication.cs
+++ src/HRMasterASP/Models/JobApplication.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace HRMasterASP.Models
 {
@@ -10,11 +11,33 @@ namespace HRMasterASP.Models
     {
         public int Id { get; set; }
         public int OfferId { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Contact Email Address")]
         public string EmailAddress { get; set; }
         public bool ContactAgreement { get; set; }
+
+        [Required]
+        [Display(Name = "CV URL")]
         public string CvUrl { get; set; }
+
+        [Required]
+        [Display(Name = "Date Of Birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [MinLength(100)]
+        public string Description { get; set; }
     }
 }
