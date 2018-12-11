d71fde6dfddec825d229f35050f0a4722df6acc3
Michał Chęciński <michal.checinski@outlook.com>
2018-12-11 18:12:07 +0100

    Add field to upload cv file

--- src/HRMasterASP/Models/JobApplicationCreateView.cs
+++ src/HRMasterASP/Models/JobApplicationCreateView.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace HRMasterASP.Models
 {
     public class JobApplicationCreateView : JobApplication
     {
         public string JobOfferName { get; set; }
+
+        [Required]
+        [Display(Name = "CV File")]
+        public IFormFile CvFile { get; set; }
     }
 }
