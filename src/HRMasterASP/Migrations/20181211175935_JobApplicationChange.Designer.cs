bf2e31bf5e121497249df6b1b2899c3b92604f0d
Michał Chęciński <michal.checinski@outlook.com>
2018-12-11 19:06:55 +0100

    Add db migration

--- src/HRMasterASP/Migrations/20181211175935_JobApplicationChange.Designer.cs
+++ src/HRMasterASP/Migrations/20181211175935_JobApplicationChange.Designer.cs
@@ -4,14 +4,16 @@ using HRMasterASP.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HRMasterASP.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20181211175935_JobApplicationChange")]
+    partial class JobApplicationChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/HRMasterASP/Migrations/20181211175935_JobApplicationChange.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HRMasterASP.Migrations
+{
+    public partial class JobApplicationChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CvUrl",
+                table: "JobApplications",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CvUrl",
+                table: "JobApplications",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
